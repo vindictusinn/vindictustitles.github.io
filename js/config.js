5faace57c5bf355944dfff2759e065cd2db3d184
vindictusinn <saniteeandduros@gmail.com>
2018-04-06 02:41:46 -0700

    dgfh

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ ItemApp.config(['$locationProvider', '$routeProvider', function ($locationProvid
   when('/list', {
     template: '<table id="titles" class="table table-striped">'+
 			'<thead class="thead-dark">'+
-			'<th>Title</th><th>Description</th><th>Obtain</th><th>STR</th><th>WIL</th><th>AGI</th><th>INT</th><th>HP</th><th>ATK</th><th>MATK</th>'+
+			'<th>Title</th><th>Description</th><th>Obtain</th><th style="width:5%">STR</th><th style="width:5%">WIL</th><th style="width:5%">AGI</th><th style="width:5%">INT</th><th style="width:5%">HP</th><th style="width:5%">ATK</th><th style="width:5%">MATK</th>'+
 		'</thead>'+
 			'<tbody>'+
 				'<tr ng-repeat="item in ItemVariable | filter : ' + filter + '" class="events{{item.Event}} removed{{item.Removed}} unreleased{{item.Unreleased}} golden{{item.Golden}} exclusive{{item.Exclusive}}  pvp{{item.PvP}}"><td>{{item.Title}}</td><td>{{item.Desc}}</td><td>{{item.Obtain}}<br/><span class="requirements">({{item.Char}} Required)</span></td><td class="str">{{item.STR}}</td><td class="wil">{{item.WILL}}</td><td class="agi">{{item.DEX}}</td><td class="int">{{item.INT}}</td><td class="hp">{{item.HP}}</td><td class="atk">{{item.ATK}}</td><td class="matk">{{item.MATK}}</td></tr>'+
